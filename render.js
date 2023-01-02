1270968ea2769eb4781246349896b61fe1b73af5
hensier <ericshen0@yeah.net>
2023-01-02 17:14:50 +0800

    1. 为部分代码块添加了注释；2. 将逐字渲染的模块单独存在一个 js 文件里。

---
+++ render.js
@@ -0,0 +1,37 @@
+$('#fireworksbtn').click(e => {
+$('#fireworksbtn').hide()
+setTimeout(() => {
+    fireworks();
+    start();
+}, 1600)
+console.log(updateConfig({
+    autoLaunch: true
+    }));
+})
+
+function fireworks() {
+    $('.page_one').addClass('hide');
+    $('.page_two').removeClass('hide');
+}
+
+function start() {
+    let str = $("#text").html();
+    let str_ = ''
+    let i = 0
+    let content = document.getElementById('contents')
+    let timer = setInterval(() => {
+        if (str_.length < str.length) {
+            if (str[i] == '<' && str[i + 1] == 'i') {
+                for (var j = 0; j < 29; j++) {
+                    str_ += str[i++]
+                }
+            }
+            str_ += str[i++]
+            content.innerHTML = '<p>' + str_ + '<span class="xx" style="opacity: 1;    color: white;">∎</span></p>'
+        }
+        else {
+            clearInterval(timer)
+            content.innerHTML = '<p>' + str_ + '</p>'
+        }
+    }, 100)
+}
